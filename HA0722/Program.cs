4fff63f06e2a62c9717966ea752756e70c006858
Hallanzinho <hallan.santos01@etec.sp.gov.br>
2022-11-07 21:58:38 -0300

    teste 2

---
+++ HA0722/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HA0722
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
